c87eae53c037166d0de2c09bed3c782a81812f38
dknapp16 <47129329+dknapp16@users.noreply.github.com>
2019-02-20 22:52:09 -0600

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        double x = 27;
+
+        System.out.printf("sqrt(%.3f) is %.3f%n", x, Math.sqrt(x));
+
+        double y = 28/5 ;
+
+
+        System.out.println(y);
+
+        int v=0;
+        int h=0;
+        double number = (double) (Math.random()*x)+ y;
+
+
+        System.out.println(number);
+
+        double result = Math.pow(2.2, 4);
+        System.out.println(result);
+        
+    }
+
+
+
+
+}
